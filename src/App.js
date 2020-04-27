7cc262ff4d218bb31eec34a4030c77048acb1172
andrewzweb <andrewzweb@gmail.com>
2020-04-27 22:59:37 +0300

    Add some features

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React from 'react'
 import TodoList from './Todo/TodoList'
 
 function App() {
-  const todos = [
-	{'id': 1, completed: false, title: 'Buy bred'},
-	{'id': 2, completed: false, title: 'Repeir cheir'},
-	{'id': 3, completed: false, title: 'Create Logo'},
-	{'id': 4, completed: false, title: 'Call Tifanni'},
-	{'id': 5, completed: false, title: 'Deploy somehing'},
-  ]
+  const [todos, setTodos] = React.useState([
+	  {'id': 1, completed: false, title: 'Buy bred'},
+	  {'id': 2, completed: true, title: 'Repeir cheir'},
+	  {'id': 3, completed: false, title: 'Create Logo'},
+	  {'id': 4, completed: false, title: 'Call Tifanni'},
+	  {'id': 5, completed: false, title: 'Deploy somehing'},
+	])
+
+  function toggleTodo(id){
+	setTodos(
+	  todos.map(todo => {
+		if (todo.id === id) {
+		  todo.completed = !todo.completed
+		}
+		return todo
+	  })
+	)
+  }
 
   return(
 	<div className='wrapper'>
 	  <h1>React Todo</h1>		
-		<TodoList todos={todos}/>	
+		<TodoList todos={todos} onToggle={toggleTodo}/>	
 	</div>
   )
 }
