e44101cc9709a8e519f449e74e56b6e4d46c2edf
andrewzweb <andrewzweb@gmail.com>
2020-04-27 21:17:55 +0300

    Create simple TodoList on react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from 'react'
+import TodoList from './Todo/TodoList'
 
 function App() {
-  return (
-    <div/>
-  );
+  const todos = [
+	{'id': 1, completed: false, title: 'Buy bred'},
+	{'id': 2, completed: false, title: 'Repeir cheir'},
+	{'id': 3, completed: false, title: 'Create Logo'},
+	{'id': 4, completed: false, title: 'Call Tifanni'},
+	{'id': 5, completed: false, title: 'Deploy somehing'},
+  ]
+
+  return(
+	<div className='wrapper'>
+	  <h1>React Todo</h1>		
+		<TodoList todos={todos}/>	
+	</div>
+  )
 }
 
-export default App;
+export default App
